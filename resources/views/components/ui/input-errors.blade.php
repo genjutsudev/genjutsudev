561b1033cf9289c9313fdfa5f060d953e7ec736a
Roma Kuzmin <thenoilty@gmail.com>
2025-06-22 17:11:10 +0300

    added lang added input-errors fixed component/email

---
+++ resources/views/components/ui/input-errors.blade.php
@@ -0,0 +1,13 @@
+@props(['messages'])
+
+@if ($messages)
+    <ul
+        {{ $attributes->merge([
+            'class' => 'm-0 py-2'
+        ]) }}
+    >
+        @foreach ((array) $messages as $message)
+            <li>{{ $message }}</li>
+        @endforeach
+    </ul>
+@endif
