0eee503b29182af28835d5334cbef0ea67ebcc4d
Roma Kuzmin <thenoilty@gmail.com>
2025-06-23 21:04:03 +0300

    fixed tmpl

--- resources/views/sections/users/show/index.blade.php
+++ resources/views/sections/users/show/index.blade.php
@@ -38,41 +38,46 @@
             <x-ui.block
                 {{-- Проверяем, что текущий пользователь пытается редактировать себя --}}
                 :disabled="$user && Auth::user()?->id !== $user->id"
-                class="card-footer d-flex align-items-center m-0">
-                <div class="flex-grow-1" style="font-size: 24px;">
-                    @foreach([
-                        'Друзья' => ['href' => '#', 'icon' => '<i class="fas fa-user-friends"></i>'],
-                        'Подписки' => ['href' => '#', 'icon' => '<i class="fa fa-rss"></i>'],
-                        'Комментарии' => ['href' => '#', 'icon' => '<i class="fa fa-comments"></i>'],
-                        'Рецензии' => ['href' => '#', 'icon' => '<i class="fa fa-paint-brush"></i>'],
-                        'Заявки' => ['href' => '#', 'icon' => '<i class="fa fa-ticket"></i>'],
-                        'Жалобы' => ['href' => '#', 'icon' => '<i class="fa fa-warning"></i>'],
-                        'Настройки' => ['href' => '/'/* route('users.show.edit.account', [$user->nid, $user->profilelink]) */, 'icon' => '<i class="fas fa-user-cog"></i>'],
-                    ] as $title => $item)
-                        <a
-                            href="{{ $item['href'] }}"
-                            @class(['me-3', 'link-azure', 'disabled' => $item['href'] === '#'])
-                            class="pe-3 link-azure"
-                            data-bs-toggle="tooltip"
-                            data-bs-placement="bottom"
-                            data-bs-original-title="{{ $title }}">
-                            {!! $item['icon'] !!}
-                        </a>
-                    @endforeach
-                </div>{{-- TODO: components/ui --}}
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"></path>
-                </svg>
-                <form id="form-search" action="/" method="post" class="m-0">
-                    @method('put')
-                    @csrf
-                    <label class="form-switch ps-3">
-                        <input disabled type="checkbox" name="user_preferences[theme]" value="1" class="form-check-input m-0 me-3" style="cursor: pointer;" onchange="this.form.submit()">
-                    </label>
-                </form>
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
-                </svg>
+                class="card-footer m-0">
+                <div class="d-flex">
+                    <div class="d-flex me-auto" style="font-size: 24px;">
+                        @foreach([
+                            'Друзья' => ['href' => '#', 'icon' => '<i class="fas fa-user-friends"></i>'],
+                            'Подписки' => ['href' => '#', 'icon' => '<i class="fa fa-rss"></i>'],
+                            'Комментарии' => ['href' => '#', 'icon' => '<i class="fa fa-comments"></i>'],
+                            'Рецензии' => ['href' => '#', 'icon' => '<i class="fa fa-paint-brush"></i>'],
+                            'Заявки' => ['href' => '#', 'icon' => '<i class="fa fa-ticket"></i>'],
+                            'Жалобы' => ['href' => '#', 'icon' => '<i class="fa fa-warning"></i>'],
+                            'Настройки' => ['href' => '/'/* route('users.show.edit.account', [$user->nid, $user->profilelink]) */, 'icon' => '<i class="fas fa-user-cog"></i>'],
+                        ] as $title => $item)
+                            <a
+                                href="{{ $item['href'] }}"
+                                @class(['me-3', 'link-azure', 'disabled' => $item['href'] === '#'])
+                                class="pe-3 link-azure"
+                                data-bs-toggle="tooltip"
+                                data-bs-placement="bottom"
+                                data-bs-original-title="{{ $title }}">
+                                {!! $item['icon'] !!}
+                            </a>
+                        @endforeach
+                    </div>{{-- TODO: components/ui --}}
+                    <div class="d-flex align-items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"></path>
+                        </svg>
+                        <form action="/" method="post" class="m-0">
+                            @method('put')
+                            @csrf
+                            <label class="form-switch ps-3">
+                                <input disabled type="checkbox" name="user_preferences[theme]" value="1" class="form-check-input m-0 me-3" style="cursor: pointer;" onchange="this.form.submit()">
+                            </label>
+                        </form>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
+                        </svg>
+                    </div>
+                </div>
             </x-ui.block>
         </div>
     </div>
